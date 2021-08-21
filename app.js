d27bf74d1084739556cf070df2a8ff562b09eb45
David Hsieh <61106644+davidyen-888@users.noreply.github.com>
2021-08-21 17:35:14 +0800

    Complete prototype

---
+++ app.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('#searchForm');
+const input = document.querySelector('#input');
+form.addEventListener('submit', async function (e) {
+    try {
+        e.preventDefault();
+        const searchTerm = form.elements.query.value;
+        // query string object
+        const config = { params: { q: searchTerm } };
+        const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+        makeImages(res.data);
+        form.elements.query.value = '';
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+const makeImages = (results) => {
+    for (let result of results) {
+        if (result.show.image) {
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
+
+const clearImages = () => {
+    const allImages = document.querySelectorAll('img');
+    for (let image of allImages) {
+        image.remove();
+    }
+}
+
+const button = document.querySelector('#clear');
+button.addEventListener('click', clearImages);
+// user can clear all images by either clicking the button or perform the next query
+input.addEventListener('keydown', clearImages);
